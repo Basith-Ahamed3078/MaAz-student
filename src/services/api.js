f29f8a89bcd1a7e2c92616a2ff73bfa332c26573
Basith-Ahamed3078 <basith@jackstechnologies.com>
2026-04-18 08:56:27 +0530

    Update api.js

--- src/services/api.js
+++ src/services/api.js
@@ -2,7 +2,8 @@ import axios from "axios";
 
 // ✅ Create axios instance
 const api = axios.create({
-  baseURL: "http://localhost:5000/api",
+  //baseURL: "http://localhost:5000/api",
+  baseURL: "https://maaz-backend-production.up.railway.app/api",
   headers: {
     "Content-Type": "application/json",
   },
